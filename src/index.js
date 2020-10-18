611619889da2ce3304b5283712e0c44f879df41b
Cole Hynes <cole.hynes1@hotmail.com>
2020-10-18 02:16:39 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 var element = React.createElement('h1', {className: 'greeting'}, 'Hello, world!');
