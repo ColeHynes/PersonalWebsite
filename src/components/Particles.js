8623cdfc639e55527d492f9381b0c923771ea101
Cole Hynes <cole.hynes1@hotmail.com>
2020-10-30 00:32:53 -0400

    Added Particle Functionality

---
+++ src/components/Particles.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Particles from 'react-particles-js';
+
+export default function Particle(props) {
+    return(
+        <div>
+            <Particles 
+            width='100'
+            height='100'
+            params={{
+                    "particles": {
+                        "number": {
+                            "value": 50
+                        },
+                        "size": {
+                            "value": 3
+                        }
+                    },
+                    "interactivity": {
+                        "events": {
+                            "onhover": {
+                                "enable": true,
+                                "mode": "repulse"
+                            }
+                        }
+                    }
+                }}
+                style={{
+                    backgroundColor: '#000000'
+                }}
+                />
+        </div>
+    )
+}
